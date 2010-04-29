8a9291a359bfe7e73886c2b117e5ca8b04d2c131
ricecake <ricecake@ricecake-lucid.(none)>
2010-04-29 10:57:46 +0800

    added unit and functional tests

--- test/functional/scores_controller_test.rb
+++ test/functional/scores_controller_test.rb
@@ -1,8 +1,39 @@
 require 'test_helper'
 
 class ScoresControllerTest < ActionController::TestCase
-  # Replace this with your real tests.
-  test "the truth" do
-    assert true
+  test "top stackrs" do
+    get :index
+	assert_response :success
+	resp = ActiveSupport::JSON.decode(@response.body)
+	#puts YAML::dump(resp)
+	assert_equal resp["today"], [
+		{"score" => { "username" => "ricecake", "lines_made" => 40 }},
+		{"score" => { "username" => "erick", "lines_made" => 30 }}
+	], "Today data is incorrect"
+	
+	assert_equal resp["all"], [
+		{"score" => { "username" => "ricecake", "lines_made" => 40 }},
+		{"score" => { "username" => "bar", "lines_made" => 35 }},
+		{"score" => { "username" => "james", "lines_made" => 34 }},
+		{"score" => { "username" => "jen", "lines_made" => 33 }},
+		{"score" => { "username" => "erick", "lines_made" => 30 }},
+		{"score" => { "username" => "foo", "lines_made" => 30 }}
+	], "All-time data is incorrect"
+	
+	assert_equal resp["week"], [
+		{"score" => { "username" => "ricecake", "lines_made" => 40 }},
+		{"score" => { "username" => "bar", "lines_made" => 35 }},
+		{"score" => { "username" => "erick", "lines_made" => 30 }},
+		{"score" => { "username" => "foo", "lines_made" => 30 }}
+	], "Week data is incorrect"
+	
+	assert_equal resp["month"], [
+		{"score" => { "username" => "ricecake", "lines_made" => 40 }},
+		{"score" => { "username" => "bar", "lines_made" => 35 }},
+		{"score" => { "username" => "james", "lines_made" => 34 }},
+		{"score" => { "username" => "jen", "lines_made" => 33 }},
+		{"score" => { "username" => "erick", "lines_made" => 30 }},
+		{"score" => { "username" => "foo", "lines_made" => 30 }}
+	], "Month data is incorrect"
   end
 end
--- test/unit/score_test.rb
+++ test/unit/score_test.rb
@@ -1,8 +1,21 @@
 require 'test_helper'
 
 class ScoreTest < ActiveSupport::TestCase
-  # Replace this with your real tests.
-  test "the truth" do
-    assert true
+  test "score should not save without username" do
+	score = Score.new
+	score.lines_made = 30
+    assert !score.save, "Score saved without username"
+  end
+  
+  test "score should not save without lines_made" do
+	score = Score.new
+	score.username = "popoy"
+    assert !score.save, "Score saved without lines made"
+  end
+  
+  test "score should not save if username is more than 30 characters" do
+	score = Score.new
+	score.username = "popoypopoypopoypopoypopoypopoypopoypopoy"
+    assert !score.save, "Score saved even if username has more than 30 chars"
   end
 end
